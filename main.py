f0d7ea61f3ecf3f67b7c44e7a502309da3f95f5f
Kmiles <71646271+Kdm2633@users.noreply.github.com>
2022-05-22 08:17:10 +0000

    Tooltip working

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ from flask import Flask, render_template
 
 app = Flask('app')
 
+
 @app.route('/')
 def map():
-    return render_template('Sorry.html')
+    return render_template('visualization.html')
 
 
 @app.route('/howtohelp')
 def howtohelp():
     return render_template('howtohelp.html')
 
+
 # @app.route('/')
 # def homepage():
 #     return render_template('index.html')
-  
 
 # Refugee Data
 refugee_data = pd.read_csv('data/UkraineRefugees.csv')
@@ -37,8 +38,12 @@ pd.set_option('display.max_rows', 200)
 
 #ploting bubbles on map
 #Arrays of country coordinates
-long = [21.01178, 19.040236, 28.468344, 19.48489, 25.005936, 37.618423, 28.054094]
-latitude = [52.22977, 47.497913, 47.203705, 48.707485, 45.83774, 55.751244, 53.543472]
+long = [
+    21.01178, 19.040236, 28.468344, 19.48489, 25.005936, 37.618423, 28.054094
+]
+latitude = [
+    52.22977, 47.497913, 47.203705, 48.707485, 45.83774, 55.751244, 53.543472
+]
 
 # # Healthcare Facilities Data
 healthcare_data = pd.read_csv('data/health_facilities.csv')
@@ -64,19 +69,22 @@ m = folium.Map(
     "https://api.mapbox.com/styles/v1/lumii/cl3g4e55100aq14qumgn4whkg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVtaWkiLCJhIjoiY2wzZzN3ZGEyMDVnZjNmcDc3Z3Jodm0wbyJ9.oTc222D0JZekjMQD_1EGYw",
     attr="mapbox")
 folium.GeoJson(data=ukr_geo["geometry"]).add_to(m)
+
+
 def plot_circles():
-  for x in range (0, len(refugee_data["Name"])):
-    folium.CircleMarker(location=[latitude[x], long[x]],
-                    radius=50,
-                    color="blue",
-                    fill_color="blue").add_to(m)
+    for x in range(0, len(refugee_data["Name"])):
+        folium.CircleMarker(location=[latitude[x], long[x]],
+                            radius=50,
+                            color="blue",
+                            fill_color="blue").add_to(m)
 
 
 plot_circles()
-# print("worked 2")
+
 custom_scale = (healthcare_data['Health_Facilities'].quantile(
     (0, 0.2, 0.4, 0.6, 0.8, 1))).tolist()
-cp = folium.Choropleth(
+
+folium.Choropleth(
     geo_data=ukr_geo,
     name="Healthcare Facilities",
     data=healthcare_data,
@@ -90,18 +98,19 @@ cp = folium.Choropleth(
     legend_name="Number of Healthcare Facilities in Each Province",
 ).add_to(m)
 
-#  # creating a state indexed version of the dataframe so we can lookup values
-# province_data_indexed = refugee_data.set_index('Region')
-  
-#   # looping thru the geojson object and adding a new property(unemployment)
-#   # and assigning a value from our dataframe
-# for s in cp.geojson.data['name_1']:
-#     s['properties']['Health_Facilities'] =   province_data_indexed.loc[s['id'], 'Health_Facilities']
-  
-#   # and finally adding a tooltip/hover to the choropleth's geojson
-# folium.GeoJsonTooltip(['Region','Health_Facilities']).add_to(cp.geojson)
 
-
+folium.features.GeoJson(ukr_geo,
+                        name='Labels',
+                        style_function=lambda x: {
+                            'color': 'transparent',
+                            'fillColor': 'transparent',
+                            'weight': 0
+                        },
+                        tooltip=folium.features.GeoJsonTooltip(
+                            fields=['name_1'],
+                            aliases=['Health Facilities'],
+                            labels=True,
+                            sticky=False)).add_to(m)
 folium.LayerControl().add_to(m)
 m.save('./templates/maps.html')
 
