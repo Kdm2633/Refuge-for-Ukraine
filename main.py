4b940f9fc9ed8e8f63883cae4cdc79348950b1c1
Kmiles <71646271+Kdm2633@users.noreply.github.com>
2022-05-22 00:18:29 +0000

    circles kinda working

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ app = Flask('app')
 def map():
     return render_template('maps.html')
 
+@app.route('/test')
+def homepage():
+    return render_template('index.html')
+
 # Refugee Data
 refugee_data = pd.read_csv('data/UkraineRefugees.csv')
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
@@ -27,41 +31,38 @@ pd.set_option('display.max_rows', 200)
 # print(refugee_data)
 
 # # Healthcare Facilities Data
-# healthcare_data = pd.read_csv('data/health_facilities.csv')
-# ukr_geo = gpd.read_file(
-#     'data/ukr_geo.json')  #administrative divisions shapefile
+healthcare_data = pd.read_csv('data/health_facilities.csv')
+ukr_geo = gpd.read_file('data/ukr_geo.json')  #administrative divisions shapefile
 
 # print(ukr_geo)
 
 # Mapping
-# m = folium.Map(
-#     location=[50.715, 24.213],
-#     zoom_start=4,
-#     min_zoom=4,
-#     max_zoom=7,
-#     tiles=
-#     "https://api.mapbox.com/styles/v1/lumii/cl3g4e55100aq14qumgn4whkg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVtaWkiLCJhIjoiY2wzZzN3ZGEyMDVnZjNmcDc3Z3Jodm0wbyJ9.oTc222D0JZekjMQD_1EGYw",
-#     attr="mapbox")
-# folium.GeoJson(data=ukr_map["geometry"]).add_to(m)
-# # Creating Markers
-# print("worked 1")
-# folium.CircleMarker(location=[50.000, 25.0000], radius=50,
-#                     color="#ffffff").add_to(m)
+m = folium.Map(
+    location=[50.715, 24.213],
+    zoom_start=4,
+    min_zoom=4,
+    max_zoom=7,
+    tiles="https://api.mapbox.com/styles/v1/lumii/cl3g4e55100aq14qumgn4whkg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVtaWkiLCJhIjoiY2wzZzN3ZGEyMDVnZjNmcDc3Z3Jodm0wbyJ9.oTc222D0JZekjMQD_1EGYw",
+    attr="mapbox")
+folium.GeoJson(data=ukr_geo["geometry"]).add_to(m)
+# Creating Markers
+print("worked 1")
+folium.CircleMarker(location=[50.000, 25.0000], radius=50,color="red",fill_color="red").add_to(m)
 # print("worked 2")
-# # folium.Choropleth(
-# #     geo_data=ukr_geo,
-# #     name="choropleth",
-# #     data=healthcare_data,
-# #     columns=["Region", "Health_Facilities"],
-# #     key_on="feature.id",
-# #     fill_color="YlGn",
-# #     fill_opacity=0.7,
-# #     line_opacity=.1,
-# #     legend_name="Healthcare Facilities",
-# # ).add_to(m)
+folium.Choropleth(
+    geo_data=ukr_geo,
+    name="choropleth",
+    data=healthcare_data,
+    columns=["Region", "Health_Facilities"],
+    key_on="feature.id",
+    fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=.1,
+    legend_name="Healthcare Facilities",
+).add_to(m)
 # print("worked 3")
 
-# #folium.LayerControl().add_to(m)
-# m.save('./templates/map.html')
+folium.LayerControl().add_to(m)
+m.save('./templates/maps.html')
 
 app.run(host='0.0.0.0', port=8080)
